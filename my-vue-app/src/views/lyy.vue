ac6543eee4ad38975f51748b944ff34726d71dd6
liyangyi123 <1091328911@qq.com>
2025-05-08 14:53:56 +0800

    message

--- my-vue-app/src/views/lyy.vue
+++ my-vue-app/src/views/lyy.vue
@@ -1,7 +1,22 @@
 <template>
     <div>
         <h1>Hello World</h1>
+        <button @click="handleClick">Click me</button>
     </div>
 </template>
+
+
+<script>
+export default {
+    name: 'Lyy',
+    methods: {
+        handleClick() {
+            console.log('Button clicked');
+        }
+    }
+
+
+}
+</script>
 
 
