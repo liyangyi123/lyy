6632b28c0488cdd5f5270ba47d2db8836f3d1376
liyangyi123 <1091328911@qq.com>
2025-05-08 15:03:58 +0800

    modify yangyi.vue

--- my-vue-app/src/views/yangyi.vue
+++ my-vue-app/src/views/yangyi.vue
@@ -2,7 +2,21 @@
     <div>
         <h1>Hello World</h1>
         <h2>Hello World</h2>
-        <h3>Hello World</h3>
+        
+        <button @click="handleClick">Click me</button>
     </div>
 </template>
+
+<script>
+export default {
+    name: 'Yangyi',
+    methods: {
+        handleClick() {
+            console.log('Button clicked');
+        }
+    }
+}
+</script>
+
+
 
